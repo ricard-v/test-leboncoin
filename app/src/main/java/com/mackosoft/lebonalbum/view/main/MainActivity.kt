42adbee84700925a98a38817db22c7950ee541c4
Vincent RICARD <mackovich.fr@gmail.com>
2020-05-10 16:13:22 +0200

    Changed app logo. Added splashscreen.

--- app/src/main/java/com/mackosoft/lebonalbum/view/main/MainActivity.kt
+++ app/src/main/java/com/mackosoft/lebonalbum/view/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mackosoft.lebonalbum.view.main
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import com.mackosoft.lebonalbum.R
 
@@ -8,6 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
 }
